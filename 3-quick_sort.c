9af1037344d39958e83e543f766d519f0b04eede
ezedksl <ezedksl@gmail.com>
2021-02-01 12:33:56 +0000

    update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,7 +7,7 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	if (!array || size <= 0)
+	if ((!array) || (size <= 0))
 		return;
 	sorting(array, 0, size - 1, size);
 }
@@ -23,9 +23,9 @@ void quick_sort(int *array, size_t size)
 int partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
-	int a = low, b = low;
-	int temp;
+	int a = 0, b = 0, temp = 0;
 
+	a = low;
 	for (b = low; b < high; b++)
 	{
 		/**
@@ -63,7 +63,7 @@ void sorting(int *array, int low, int high, size_t size)
 
 	if (low < high)
 	{
-		p = partition(array, 0, high, size);
+		p = partition(array, low, high, size);
 		sorting(array, low, p - 1, size);
 		sorting(array, p + 1, high, size);
 	}
